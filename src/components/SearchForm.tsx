4b1c3e22b8809aee3947b0d4bd5c80421e7e6a88
abiney1321 <abiney1321@gmail.com>
2025-02-24 19:26:10 +0000

    data fetching completed

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react";
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+
+  console.log(jobItems);
 
   useEffect(() => {
     if (!searchText) return;
 
-    fetch(
-      `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-    );
+    const fetchData = async function () {
+      const res = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await res.json();
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
   }, [searchText]);
 
   return (
