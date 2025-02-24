89b20d0825f88c687a6053b9e123cd22287233e9
abiney1321 <abiney1321@gmail.com>
2025-02-24 00:11:29 +0000

    data fetching

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    fetch(
+      `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+    );
+  }, [searchText]);
 
   return (
     <form
