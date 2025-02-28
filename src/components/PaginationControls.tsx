8852f623ff389ed9e093d46228749d4d3331367c
abiney1321 <abiney1321@gmail.com>
2025-02-28 22:10:41 +0000

    pagination buttons

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,16 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
 export default function PaginationControls() {
-  return <section className="pagination"></section>;
+  return (
+    <section className="pagination">
+      <button className="pagination__button">
+        <ArrowLeftIcon />
+        Page 1
+      </button>
+      <button className="pagination__button">
+        Page 3
+        <ArrowRightIcon />
+      </button>
+    </section>
+  );
 }
