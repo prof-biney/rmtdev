e3b56d2557a9d8949df5968c8efbc190a38c684a
abiney1321 <abiney1321@gmail.com>
2025-03-02 23:58:10 +0000

    context api for bookmark

---
+++ src/contexts/BookmarksContextProvider.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function BookmarksContextProvider({ children }) {
+  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkedIds.includes(id)) {
+      setBookmarkedIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setBookmarkedIds((prev) => [...prev, id]);
+    }
+  };
+
+  return <div>{children}</div>;
+}
