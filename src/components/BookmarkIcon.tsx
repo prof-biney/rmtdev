b38ba7b50f4221c609f56d05d53f07ecc0a4539b
abiney1321 <abiney1321@gmail.com>
2025-03-02 23:51:13 +0000

    unfilling all bookmark icons

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -3,7 +3,7 @@ import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 export default function BookmarkIcon() {
   return (
     <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+      <BookmarkFilledIcon className="" />
     </button>
   );
 }
