4989c58b12d1256970ee620423ef81317136cd8c
abiney1321 <abiney1321@gmail.com>
2025-03-06 19:06:00 +0000

    refactored

--- src/contexts/BookmarksContextProvider.tsx
+++ src/contexts/BookmarksContextProvider.tsx
@@ -1,10 +1,12 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import { useLocalStorage } from "../lib/hooks";
 
 export const BookmarksContext = createContext(null);
 
 export default function BookmarksContextProvider({ children }) {
-  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(() =>
-    JSON.parse(localStorage.getItem("bookmarkedIds") || "[]")
+  const [bookmarkedIds, setBookmarkedIds] = useLocalStorage(
+    "bookmarkedIds",
+    []
   );
 
   const handleToggleBookmark = (id: number) => {
@@ -14,10 +16,6 @@ export default function BookmarksContextProvider({ children }) {
       setBookmarkedIds((prev) => [...prev, id]);
     }
   };
-
-  useEffect(() => {
-    localStorage.setItem("bookmarkedIds", JSON.stringify(bookmarkedIds));
-  }, [bookmarkedIds]);
 
   return (
     <BookmarksContext.Provider
