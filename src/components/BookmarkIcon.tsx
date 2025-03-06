3bedb4afdbfb20d9d5d7a28c75b0286a5386818b
abiney1321 <abiney1321@gmail.com>
2025-03-06 14:00:06 +0000

    bookmark icon usage

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,20 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useContext } from "react";
+import { BookmarksContext } from "../contexts/BookmarksContextProvider";
 
-export default function BookmarkIcon() {
+type BookmarkIconProps = {
+  id: number;
+};
+
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
+  console.log(bookmarkedIds);
+
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="" />
+    <button onClick={() => handleToggleBookmark(id)} className="bookmark-btn">
+      <BookmarkFilledIcon
+        className={bookmarkedIds.includes(id) ? "filled" : ""}
+      />
     </button>
   );
 }
