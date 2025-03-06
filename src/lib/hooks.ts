489f4cac47b38c768d7874c98b9d936e47038d60
abiney1321 <abiney1321@gmail.com>
2025-03-06 19:24:27 +0000

    typing custom hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -123,8 +123,11 @@ export function useDebounce<T>(value: T, delay = 500): T {
   return debouncedValue;
 }
 
-export function useLocalStorage<T>(key: string, initialValue: T) {
-  const [value, setValue] = useState<number[]>(() =>
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [value, setValue] = useState(() =>
     JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
   );
 
