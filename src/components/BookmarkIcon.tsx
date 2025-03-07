412dc7b2d3ac5f1a62a035901e14b4f31e06a8ae
abiney1321 <abiney1321@gmail.com>
2025-03-07 21:21:28 +0000

    refactoring context api usage

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,18 +1,12 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
-import { useContext } from "react";
-import { BookmarksContext } from "../contexts/BookmarksContextProvider";
+import { useBookmarksContext } from "../contexts/BookmarksContextProvider";
 
 type BookmarkIconProps = {
   id: number;
 };
 
 export default function BookmarkIcon({ id }: BookmarkIconProps) {
-  const context = useContext(BookmarksContext);
-  if (!context) {
-    throw new Error("Component must be used within BookmarksContextProvider");
-  }
-
-  const { bookmarkedIds, handleToggleBookmark } = context;
+  const { bookmarkedIds, handleToggleBookmark } = useBookmarksContext();
 
   return (
     <button
