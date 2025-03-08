b9c38d408ad4b9bda8ac1bff8a41c3f93fb6f16b
abiney1321 <abiney1321@gmail.com>
2025-03-08 23:37:13 +0000

    bookmarks popover

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,3 +1,12 @@
+import { useBookmarksContext } from "../lib/hooks";
+import JobList from "./JobList";
+
 export default function BookmarksPopover() {
-  return <div className="bookmarks-popover"></div>;
+  const { boookmarkedIds, handleToggleBookmark } = useBookmarksContext();
+
+  return (
+    <div className="bookmarks-popover">
+      <JobList jobItems={[]} isLoading={false} />
+    </div>
+  );
 }
